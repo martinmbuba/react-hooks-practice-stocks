9725ddf00e7e10fb5f081b1f5605bc4aacd6bf12
martinmbuba <martinmbuba4@gmail.com>
2025-07-25 22:56:18 +0300

    complete

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolioStocks, setPortfolioStocks] = useState([]);
+  const [sortBy, setSortBy] = useState("Alphabetically");
+  const [filterBy, setFilterBy] = useState("All");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((res) => res.json())
+      .then((data) => setStocks(data));
+  }, []);
+
+  function handleBuyStock(stock) {
+    if (!portfolioStocks.find((s) => s.id === stock.id)) {
+      setPortfolioStocks([...portfolioStocks, stock]);
+    }
+  }
+
+  function handleSellStock(stock) {
+    setPortfolioStocks(portfolioStocks.filter((s) => s.id !== stock.id));
+  }
+
+  function handleSortChange(sortValue) {
+    setSortBy(sortValue);
+  }
+
+  function handleFilterChange(filterValue) {
+    setFilterBy(filterValue);
+  }
+
+  function getFilteredAndSortedStocks() {
+    let filteredStocks = stocks;
+    if (filterBy !== "All") {
+      filteredStocks = stocks.filter((stock) => stock.type === filterBy);
+    }
+
+    if (sortBy === "Alphabetically") {
+      filteredStocks = [...filteredStocks].sort((a, b) =>
+        a.ticker.localeCompare(b.ticker)
+      );
+    } else if (sortBy === "Price") {
+      filteredStocks = [...filteredStocks].sort((a, b) => a.price - b.price);
+    }
+
+    return filteredStocks;
+  }
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar
+        sortBy={sortBy}
+        onSortChange={handleSortChange}
+        filterBy={filterBy}
+        onFilterChange={handleFilterChange}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer
+            stocks={getFilteredAndSortedStocks()}
+            onStockClick={handleBuyStock}
+          />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer
+            portfolioStocks={portfolioStocks}
+            onStockClick={handleSellStock}
+          />
         </div>
       </div>
     </div>
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-function Stock() {
+function Stock({ stock, onClick }) {
   return (
-    <div>
+    <div onClick={() => onClick(stock)} style={{ cursor: "pointer" }}>
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+          <h5 className="card-title">{stock.name}</h5>
+          <p className="card-text">${stock.price.toFixed(2)}</p>
         </div>
       </div>
     </div>
   );
 }
+
 export default Stock;
